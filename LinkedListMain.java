55300bdaf23936a95a5df0edc68a7fc585c7db42
sanjana8100 <sanjanams812@gmail.com>
2023-03-20 19:22:13 +0530

    added: UC2

--- LinkedListMain.java
+++ LinkedListMain.java
@@ -3,5 +3,11 @@ package com.bridgelabz.LinkedList;
 public class LinkedListMain {
     public static void main(String[] args) {
         LinkedList<Integer> linkedList1= new LinkedList<>();
+
+        linkedList1.push(70);
+        linkedList1.push(30);
+        linkedList1.push(56);
+
+        linkedList1.show();
     }
 }
